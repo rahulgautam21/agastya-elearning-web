3a0e4f192966bbb02fb5d6768198e744fe9d132d
Parvesh Mamgain <mamgainparvesh@gmail.com>
2020-08-13 14:22:50 +0530

    Adjusted Course Detail Page

--- src/app/models/topic.model.ts
+++ src/app/models/topic.model.ts
@@ -1,9 +1,11 @@
 import { SubTopic } from './sub-topic.model';
+import { Category } from './category.model';
 
 export interface Topic {
   id: number;
   name: string;
   created_at: Date;
   updated_at: Date;
+  categories: Category[];
   subTopics: SubTopic[];
 }
