1536c2ded8d76b96f50a216f5681b6919b9b9275
abhay namdeo <abhay.namdeo@gmail.com>
2020-09-25 00:12:33 +0530

    Login After Header Hide Issue

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -13,7 +13,6 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
   error: any;
-  user: CurrentUser = null;
   loginForm: any;
 
   constructor(
@@ -28,8 +27,6 @@ export class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
-
-    this.user = this.auth.currentUserValue;
   }
 
   onCancel() {
@@ -42,7 +39,6 @@ export class LoginComponent implements OnInit {
     const { username, password } = this.loginForm.value;
     this.auth.login(username, password).subscribe(
       (user) => {
-        this.user = user;
         this.dialogRef.close();
       },
       (error) => {
